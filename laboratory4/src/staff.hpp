53fd7806517c18c6962852fe9452d599d1cbdf7a
Borys O <Borys O@10.16.152.227>
2013-11-13 09:21:19 -0500

    something happened/

---
+++ laboratory4/src/staff.hpp
@@ -0,0 +1,23 @@
+/*
+ * staff.hpp
+ *
+ *  Created on: Nov 13, 2013
+ *      Author: Borys O
+ */
+
+#ifndef STAFF_HPP_
+#define STAFF_HPP_
+
+class staff {
+
+	//this should show up here
+int x, y;
+
+//please work
+
+};
+
+
+
+
+#endif /* STAFF_HPP_ */
